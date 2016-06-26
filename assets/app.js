21cc02826f0e196efb3b62e0b0b9075aecc8b048
mrReiha <mr.reiha@gmail.com>
2016-06-26 20:02:25 +0430

    Added demo page

---
+++ assets/app.js
@@ -0,0 +1,49 @@
+;!( function( w, d ) {
+
+    'use strict';
+
+    var get = function( s ) { return d.querySelector( s ); },
+        getAll = function( s ) { return d.querySelectorAll( s ); },
+
+        changeTipIt = function( e ) {
+
+            tipLM.setAttribute( 'data-tipit-content', inputLM.value );
+
+            liveTipLM.innerHTML = 'Hover me Again!'
+
+        },
+
+        resetTipLM = function( e ) {
+
+            liveTipLM.innerHTML = 'Hover me';
+
+        },
+
+        handleTipIt = function( e ) {
+
+            if ( this.className.indexOf( 'show' ) >= 0 )
+                manTipLM.showTipit();
+            else
+                manTipLM.hideTipit();
+
+        },
+
+        liveTipLM = get( '.live .ex span' ),
+        inputLM = get( '.live input' ),
+
+        manTipLM = get( '.man .ex span' ),
+
+        buttons = getAll( 'button' ),
+        buttonsLen = buttons.length,
+
+        len;
+
+    inputLM.oninput = changeTipIt;
+    liveTipLM.onmouseout = resetTipLM;
+
+    len = buttonsLen;
+
+    while ( len-- )
+        buttons[ len ].onclick = handleTipIt;
+
+})( this, document );
